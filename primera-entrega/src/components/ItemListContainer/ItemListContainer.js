44dc9c196082237d0eb8f9a11d1107ff5b9ca738
santimolina <molan_24@hotmail.com>
2023-03-14 23:39:00 -0300

    navbar

---
+++ primera-entrega/src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,13 @@
+
+export const ItemListContainer = ({greeting}) => {
+
+
+    return (
+        <div className="list-container">
+            <h2 className="list-container__title">ItemListContainer</h2>
+            <hr/>
+
+            <p>{greeting}</p>
+        </div>
+    )
+}
